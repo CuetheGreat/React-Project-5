6b98e9f627decf75e7ae1093be15b96340653bba
CuetheGreat <c.stewart89@gmail.com>
2022-04-15 11:49:07 -0400

    Added backend user/auth flow

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(username: 'TestUser', password: 'password', bio: 'Something interesting about test user', avatar: 'https://thumbs.dreamstime.com/b/image-smiling-man-who-has-idea-handsome-bearded-guy-sweater-just-came-up-great-thought-white-background-134071461.jpg')
