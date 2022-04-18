900c8ad433a166cd3992b9e0ec1768a6c75f73f2
CuetheGreat <c.stewart89@gmail.com>
2022-04-18 15:56:35 -0400

    Updated drink detail page

--- frontend/src/containers/DrinkPage.js
+++ frontend/src/containers/DrinkPage.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-const DrinkPage = (props)  => {
-    let { slug } = useParams()
+const DrinkPage = props => {
+  let { slug } = useParams()
+  const drink = props.drinks.filter(d => d.slug == slug)[0]
 
-    const drink = props.drinks.filter(d => d.slug == slug)
-
-    return (
-      <div>
-
-      DrinkPage
-
+  console.log(drink)
+  return (
+    <div>
+      <div className='drink-name'>
+        <h1>{drink.name}</h1>
       </div>
-    )
-
+      <div style={{ display: 'flex' }}>
+        <img src={drink.image} />
+        <div className='drink-info'>
+          <p className='drink-description'>{drink.description}</p>
+          <ul>
+            {drink.ingredients.map((i, index) => (
+              <li key={index}>{`${i.measure} ${i.kind}`}</li>
+            ))}
+          </ul>
+          <div>
+            <p>{drink.glass}</p>
+            <p>{drink.garnish}</p>
+          </div>
+          <div>
+            {drink.instructions.map((i, index) => (
+              <li key={index}>{`${i.step}`}</li>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
+// instructions: (3) [{…}, {…}, {…}]
 
 export default DrinkPage
