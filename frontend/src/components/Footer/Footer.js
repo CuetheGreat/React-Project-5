1991d14d010f35f20ad6b242498edebd219de23d
CuetheGreat <c.stewart89@gmail.com>
2022-05-23 09:45:57 -0400

    Minor Changes

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,21 +5,31 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-  <footer className='footer'>
-    <Row>
-      <Col>
-        <h2>About Us</h2>
-        <Link to='#'>Aim</Link>
-        <Link to='#'>Testimonials</Link>
-      </Col>
-      <Col>
-        <h2>Services</h2>
-        <Link to='#'>Drinks</Link>
-        <Link to='#'>New Drink</Link>
-        <Link to='#'>Profile</Link>
-      </Col>
-    </Row>
-  </footer>)
+    <footer className='footer'>
+      <Row>
+        <Col>
+          <h2>About Us</h2>
+          <Link to='#'>Aim</Link>
+          <Link to='#'>Testimonials</Link>
+        </Col>
+        <Col>
+          <h2>Services</h2>
+          <ul>
+            <li>
+              <Link to='#'>Drinks</Link>
+            </li>
+            <li>
+              <Link to='#'>New Drink</Link>
+            </li>
+
+            <li>
+              <Link to='#'>Profile</Link>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </footer>
+  )
 }
 
 export default Footer
