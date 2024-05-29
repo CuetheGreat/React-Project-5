0d29b0ab6a6719add72f4a2789f02e34ef1c91bc
Clay Stewart <c.stewart89@gmail.com>
2024-05-28 23:02:18 -0400

    Refactoring drinkThunk.js

--- frontend/src/feature/Drinks/drinkThunk.js
+++ frontend/src/feature/Drinks/drinkThunk.js
@@ -1,34 +1,52 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const fetchAllDrinks = createAsyncThunk('drinks/fetchAllDrinks', async () => {
-  let res = await fetch('http://localhost:3000/api/v1/drinks').then(data =>
-    data.json()
-  )
-  return res
-})
+const API_BASE_URL = 'http://localhost:3000/api/v1';
 
-const fetchDrinkBySlug = createAsyncThunk(
-  'drinks/fetchDrinkBySlug',
-  async (slug) => {
-
-    let res = await fetch(`http://localhost:3000/api/v1/drinks/${slug}`).then( data => data.json())
-    console.log('Async:',res)
-    return res
+const fetchAllDrinks = createAsyncThunk('drinks/fetchAllDrinks', async (_, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/drinks`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch drinks');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
-)
+});
 
-const postNewDrink = createAsyncThunk('drinks/postNewDrink', async (reqData) => {
-  const post = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify(reqData)
+const fetchDrinkBySlug = createAsyncThunk('drinks/fetchDrinkBySlug', async (slug, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/drinks/${slug}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch drink with slug: ${slug}`);
+    }
+    const data = await response.json();
+    console.log('Async:', data);
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
+});
 
-  let res = await fetch('http://localhost:3000/api/v1/drinks', post).then(data => data.json())
-  return res
-})
+const postNewDrink = createAsyncThunk('drinks/postNewDrink', async (reqData, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/drinks`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(reqData),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to post new drink');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message);
+  }
+});
 
-export { fetchAllDrinks, fetchDrinkBySlug, postNewDrink }
+export { fetchAllDrinks, fetchDrinkBySlug, postNewDrink };
