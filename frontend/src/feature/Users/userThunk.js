c217ca707303e67d10f42266e5863782a0d980cc
Clay Stewart <c.stewart89@gmail.com>
2024-05-28 22:53:20 -0400

    Refactor userThunk.js

--- frontend/src/feature/Users/userThunk.js
+++ frontend/src/feature/Users/userThunk.js
@@ -1,59 +1,81 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const token = !!sessionStorage.getItem('jwt')
-  ? sessionStorage.getItem('jwt')
-  : null
+const API_BASE_URL = 'http://127.0.0.1:3000/api/v1';
 
-const get = {
-  method: 'GET',
+const token = sessionStorage.getItem('jwt');
+
+const getHeaders = (withAuth = false) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  if (withAuth && token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+  return headers;
+};
+
+const signIn = (data) => ({
+  method: 'POST',
   headers: {
-    'CONTENT-TYPE': 'application/json',
-    Authorization: `Bearer ${token}`
+    ...getHeaders(true),
+    Accept: 'application/json',
+  },
+  body: JSON.stringify(data),
+});
+
+const signUp = (data) => ({
+  method: 'POST',
+  headers: {
+    ...getHeaders(),
+    Accept: 'application/json',
+  },
+  body: JSON.stringify(data),
+});
+
+const fetchConfig = {
+  method: 'GET',
+  headers: getHeaders(true),
+};
+
+const login = createAsyncThunk('auth/login', async (reqData, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/login`, signIn(reqData));
+    if (!response.ok) {
+      throw new Error('Failed to login');
+    }
+    const data = await response.json();
+    sessionStorage.setItem('jwt', data.jwt);
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
-}
+});
 
-const signIn = data => {
-  return {
-    method: 'POST',
-    headers: {
-      'CONTENT-TYPE': 'application/json',
-      ACCEPT: 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-    body: JSON.stringify(data)
+const currentUser = createAsyncThunk('user/currentUser', async (_, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/profile`, fetchConfig);
+    if (!response.ok) {
+      throw new Error('Failed to fetch current user');
+    }
+    const data = await response.json();
+    console.log('Thunk:', data);
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
-}
+});
 
-const signUp = data => {
-  return {
-    method: 'POST',
-    headers: {
-      'CONTENT-TYPE': 'application/json',
-      ACCEPT: 'application/json',
-    },
-    body: JSON.stringify(data)
+const newUser = createAsyncThunk('user/newUser', async (newUserData, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/signup`, signUp(newUserData));
+    if (!response.ok) {
+      throw new Error('Failed to sign up');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message);
   }
-}
+});
 
-const login = createAsyncThunk('', async reqData => {
-  let res = await fetch(
-    'http://127.0.0.1:3000/api/v1/login',
-    signIn(reqData)
-  ).then(data => data.json())
-  sessionStorage.setItem('jwt', res.jwt)
-  return res
-})
-
-const currentUser = createAsyncThunk('', async () => {
-  let res = await fetch('http://127.0.0.1:3000/api/v1/profile', get).then(
-    data => data.json()
-  )
-  console.log('Thunk:', res)
-  return res
-})
-
-const newUser = createAsyncThunk('', async (newUserData) => {
-  let res = await fetch('', signUp(newUserData)).then(data => data.json())
-  return res
-})
-export { login, currentUser, newUser }
+export { login, currentUser, newUser };
