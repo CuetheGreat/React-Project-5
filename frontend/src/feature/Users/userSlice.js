b0284cbddfbe38539890547b94198a5d426ce67a
Clay Stewart <c.stewart89@gmail.com>
2024-05-28 22:58:21 -0400

    Update userSlice.js to account for loading states

--- frontend/src/feature/Users/userSlice.js
+++ frontend/src/feature/Users/userSlice.js
@@ -1,29 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { currentUser, login } from './userThunk'
+import { createSlice } from '@reduxjs/toolkit';
+import { currentUser, login } from './userThunk';
 
 const initialState = {
-  user: null
-}
+  user: null,
+  loading: false,
+  error: null,
+};
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    logout: (state, action) => {
-      sessionStorage.clear()
-      return { ...state, user: null }
-    }
-  },
-  extraReducers: {
-    [login.fulfilled]: (state, action) => {
-      return { ...state, user: action.payload.user }
+    logout: (state) => {
+      sessionStorage.clear();
+      state.user = null;
     },
-    [currentUser.fulfilled]: (state, action) => {
-      console.log('Payload:',action.payload.user)
-      return { ...state, user: action.payload.user }
-    }
-  }
-})
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(currentUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(currentUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+      })
+      .addCase(currentUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
 
-export default userSlice.reducer
-export const { logout } = userSlice.actions
+export default userSlice.reducer;
+export const { logout } = userSlice.actions;
