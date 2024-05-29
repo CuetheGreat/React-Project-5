af5fa23800f349c1cc1b8d261f4b00474c354ca4
Clay Stewart <c.stewart89@gmail.com>
2024-05-28 23:03:55 -0400

    update drinkSlice.js with builder notation handling loading states

--- frontend/src/feature/Drinks/drinkSlice.js
+++ frontend/src/feature/Drinks/drinkSlice.js
@@ -1,24 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { fetchAllDrinks, fetchDrinkBySlug, postNewDrink } from './drinkThunk'
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchAllDrinks, fetchDrinkBySlug, postNewDrink } from './drinkThunk';
 
-const initialState = { drinks: [], currentDrink: {} }
+const initialState = {
+  drinks: [],
+  currentDrink: {},
+  loading: false,
+  error: null,
+};
 
-let drinksSlice = createSlice({
+const drinksSlice = createSlice({
   name: 'drinks',
   initialState,
   reducers: {},
-  extraReducers: {
-    [fetchAllDrinks.fulfilled]: (state, action) => {
-      return { ...state, drinks: action.payload }
-    },
-    [fetchDrinkBySlug.fulfilled]: (state, action) => {
-      return { ...state, currentDrink: action.payload.drinks[0] }
-    },
-    [postNewDrink.fulfilled]: (state, action) => {
-      return { ...state, drinks: state.drinks.concat(action.payload.drinks[0])}
-    }
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllDrinks.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchAllDrinks.fulfilled, (state, action) => {
+        state.loading = false;
+        state.drinks = action.payload;
+      })
+      .addCase(fetchAllDrinks.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(fetchDrinkBySlug.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchDrinkBySlug.fulfilled, (state, action) => {
+        state.loading = false;
+        state.currentDrink = action.payload.drinks[0];
+      })
+      .addCase(fetchDrinkBySlug.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(postNewDrink.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(postNewDrink.fulfilled, (state, action) => {
+        state.loading = false;
+        state.drinks = state.drinks.concat(action.payload.drinks[0]);
+      })
+      .addCase(postNewDrink.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
 
-  }
-})
-
-export default drinksSlice.reducer
+export default drinksSlice.reducer;
